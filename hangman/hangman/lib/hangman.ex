ffac8b6c4d5d75aa486cd8a775cc8e995c57763b
nealbpm <nealbpm6@gmail.com>
2022-01-05 20:55:59 -0500

    Implementation package and module Game included. Dependency to add Dictionary module for Hangman done.

--- hangman/hangman/lib/hangman.ex
+++ hangman/hangman/lib/hangman.ex
@@ -1,7 +1,16 @@
 defmodule Hangman do
-  def new_game do
+  alias Hangman.Impl.Game
 
-  end
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type game  :: any()
+  @type tally :: %{
+    turns_left: integer(),
+    game_state: state,
+    letters: list(String.t),
+    used: list(String.t),
+  }
+  defdelegate new_game(), to:  Game
+
 
   def make_move(game, guess) do
   end
