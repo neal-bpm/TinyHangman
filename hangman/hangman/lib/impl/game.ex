c3b741fb0d91808eec7be1bba3c202dc00ee067b
nealbpm <nealbpm6@gmail.com>
2022-01-09 22:55:13 -0500

    Letter revealing changes and test sequences

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -43,13 +43,30 @@ defmodule Hangman.Impl.Game do
 
   def accept_guess(game, guess, _already_used_letter) do
     %{game | used: MapSet.put(game.used, guess) }
+    |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  defp score_guess(game, _good_guess = true) do
+    # guessed all letters? -> :won | :good_guess
+    new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{ game | game_state: new_state }
+  end
+
+  defp score_guess(game = %{ turns_left: 1}, _bad_guess) do
+    # turns_left == 1 -> lost | dec turns_left, :bad_guess
+    %{ game | game_state: :lost , turns_left: 0}
+  end
+
+  defp score_guess(game, _bad_guess) do
+    # turns_left == 1 -> lost | dec turns_left, :bad_guess
+    %{ game | game_state: :bad_guess, turns_left: game.turns_left - 1 }
   end
 
   defp tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list |> Enum.sort,
     }
   end
@@ -58,5 +75,15 @@ defmodule Hangman.Impl.Game do
     { game, tally(game)}
   end
 
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
+  defp maybe_won(true), do: :won
+  defp maybe_won(_),    do: :good_guess
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_, _letter), do: "_"
 
 end
