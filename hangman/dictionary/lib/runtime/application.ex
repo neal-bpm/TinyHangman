5e5e2131b7031e1736c4d354990b11c8f4e61102
nealbpm <nealbpm6@gmail.com>
2022-01-14 23:22:19 -0500

    Change for supervisor responsibility

--- hangman/dictionary/lib/runtime/application.ex
+++ hangman/dictionary/lib/runtime/application.ex
@@ -3,7 +3,14 @@ defmodule Dictionary.Runtime.Application do
   use Application
 
   def start(_start_type, _start_args) do
-    Dictionary.Runtime.Server.start_link()
+    children = [
+      {Dictionary.Runtime.Server, []}
+    ]
+    options = [
+      name: Dictionary.Runtime.Supervisor,
+      strategy: :one_for_one,
+    ]
+    Supervisor.start_link(children, options)
   end
 
 end
--- hangman/dictionary/lib/runtime/server.ex
+++ hangman/dictionary/lib/runtime/server.ex
@@ -2,14 +2,20 @@ defmodule Dictionary.Runtime.Server do
 
   @type t :: pid()
   @me __MODULE__
+
+  use Agent
+
   alias Dictionary.Impl.Wordlist
 
 
-  def start_link() do
+  def start_link(_) do
     Agent.start_link(&Wordlist.word_list/0, name: @me)
   end
 
   def random_word() do
+    if :rand.uniform() < 0.33 do
+      Agent.get(@me, fn _-> exit(:boom) end)
+    end
     Agent.get(@me , &Wordlist.random_word/1)
   end
 
